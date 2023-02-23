8d05439c406954f6c5b00ae4a779d8557dab2152
LucasAlveeeees <118401882+LucasAlveeeees@users.noreply.github.com>
2023-02-22 21:29:06 -0300

    Update IF-ELSE.py

--- IF-ELSE.py
+++ IF-ELSE.py
@@ -7,7 +7,7 @@ nota_3 = float(input("Qual a nota 3?"))
 nota_final = (nota_1+nota_2+nota_3)/3
 aproveitamento = (nota_final)*10
 nota_maxima = 10
-if nota_1 or nota_2 or nota_3 > nota_maxima:
+if nota_final > nota_maxima:
     print("algumas de suas notas foram digitadas incorretamente sua nota 1 foi {}, sua nota 2 foi {},sua nota 3 foi {}, verifique qual nota foi digitada maior que a nota maxima permitida, nota maxima:{}".format(nota_1,nota_2,nota_3,nota_maxima))
 elif nota_final == 10:
     print("Parabéns você foi muito bem!!, sua nota foi {} e seu aproveitamento foi {}%".format(nota_final,aproveitamento))
